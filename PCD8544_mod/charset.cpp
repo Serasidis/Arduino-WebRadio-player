761c474a9c279a6ad97d283d234dd101dc27e382
Vassilis Serasidis <avrsite@yahoo.gr>
2014-10-10 20:03:59 +0300

    Update charset.cpp

--- PCD8544_mod/charset.cpp
+++ PCD8544_mod/charset.cpp
@@ -20,6 +20,10 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ 
+ Modified by Vassilis Serasidis for working with 3X6 pixels font
+ Now the LCD can display up to 21 characters per line (total 6 lines).
+ 
  */
 
 
